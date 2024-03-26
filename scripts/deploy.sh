00aac3080291a7b2c0c6f1784c665bc1770871ff
soogoori <soobin0209@naver.com>
2024-03-26 21:33:08 +0900

    🐛fix: deploy.sh, dockerfile 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,15 @@
 sudo docker ps -a -q --filter "name=app" | grep -q . && docker stop app && docker rm app | true
 
 # 기존 이미지 삭제
-sudo docker rmi pgmjun/cicdstudy:latest
+sudo docker rmi soogoori99/devlife:latest
 
 # 도커 허브에서 이미지 pull --> 태그가 latest
 # 도커허브ID/도커허브REPOSITORY:TAG
-sudo docker pull pgmjun/cicdstudy:latest
+sudo docker pull soogoori99/devlife:latest
 
 # 도커 컨테이너 실행 --> 호스트의 8080 포트와 컨테이너의 8080 포트를 연결 & 컨테이너에 환경변수 파일 읽어옴
 # --env-file --> 환경변수를 저장할 파일명 => 운영용 EC2에 Secrets Manager 사용하기 위해서!
-docker run -d -p 8080:8080 --env-file=env_list.txt --name app pgmjun/cicdstudy:latest
+docker run -d -p 8080:8080 --env-file=env_list.txt --name app soogoori99/devlife:latest
 
 # 사용하지 않는 불필요한 이미지(=dangling 이미지) 삭제
 docker rmi -f $(docker images -f "dangling=true" -q) || true
