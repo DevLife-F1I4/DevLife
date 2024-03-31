a8db7da96f42c3fc531690a81881e946bf93aa37
cmleem <coals722@naver.com>
2024-03-31 22:21:04 +0900

    ✨ FEAT #10: Comment 관련 CRUD 기능 구현(User 권한 확인 제외)

---
+++ src/main/java/com/example/devlife/repository/board/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.example.devlife.repository.board;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.devlife.entity.Board;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
