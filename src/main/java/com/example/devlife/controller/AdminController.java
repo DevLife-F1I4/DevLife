446631e5262f30e78fa70fb4f286bb1ed1ebbe71
cmleem <coals722@naver.com>
2024-04-01 22:07:16 +0900

    ✨ FEAT #21: 관리자 댓글 삭제 기능 구현(Admin 권한 확인 제외)

--- src/main/java/com/example/devlife/controller/AdminController.java
+++ src/main/java/com/example/devlife/controller/AdminController.java
@@ -7,38 +7,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.devlife.dto.UpdateCommentRequest;
 import com.example.devlife.dto.UpdateUserGradeRequest;
 import com.example.devlife.entity.User;
 import com.example.devlife.service.admin.AdminService;
+import com.example.devlife.service.comment.CommentService;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RequestMapping("/api/admin")
 @Controller
 public class AdminController {
 	private final AdminService adminService;
+	private final CommentService commentService;
 
-	public AdminController(AdminService adminService) {
-		this.adminService = adminService;
-	}
 
-	@DeleteMapping("/api/admin/user/{providerId}")
+	@DeleteMapping("/user/{providerId}")
 	public void deleteUser(@PathVariable(value = "providerId") String providerId) {
 		//TODO 1. 관리자인지 확인
 		adminService.delete(providerId);
 	}
 
-	@GetMapping("/api/admin/user/{providerId}")
+	@GetMapping("/user/{providerId}")
 	public ResponseEntity<User> getUserInfo(@PathVariable(value = "providerId") String providerId){
 		//TODO 1. 관리자 권한 여부 확인
 		User user = adminService.getUser(providerId);
 		return ResponseEntity.ok().body(user);
 	}
 
-	@PatchMapping("/api/admin/user/{providerId}")
+	@PatchMapping("/user/{providerId}")
 	public ResponseEntity<User> updateUserGrade(@PathVariable(value = "providerId") String providerId, @RequestBody
 	UpdateUserGradeRequest request){
 		//TODO 1. 관리자 권한 여부 확인
 		User user = adminService.updateUserGrade(providerId, request);
 		return ResponseEntity.ok().body(user);
+	}
+
+	@DeleteMapping("/comment/{commentId}")
+	public void deleteComment(@PathVariable(value = "commentId") Long commentId){
+		//TODO 1. 관리자 권한 여부 확인
+		commentService.deleteComment(commentId);
 	}
 }
