d44786dff90c487fce6a8377c181e099afe0944e
intotheseon <intotheseon@gmail.com>
2024-04-07 00:43:53 +0900

    🐛 fix #51: 수정, 삭제버튼 오류 해결

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -1,7 +1,51 @@
-const editBtn = document.getElementById('edit-btn');
-const deleteBtn = document.getElementById('delete-btn');
+// const editBtn = document.getElementById('edit-btn');
+// const deleteBtn = document.getElementById('delete-btn');
+//
+// editBtn.addEventListener('click', () => {
+//     const editUrl = location.href + '/update';
+// console.log('clicked');
+//     // GET 요청 전송
+//     fetch(editUrl, {
+//         method: 'GET',
+//     })
+//         .then((response) => {
+//             if (response.status === 200) {
+//                 location.href = editUrl;
+//             } else {
+//                 alert('수정 실패: ' + response.statusText);
+//             }
+//         })
+//         .catch((error) => {
+//             console.error(error);
+//             alert('오류 발생. 다시 시도해주세요.');
+//         });
+// });
+//
+// deleteBtn.addEventListener('click', () => {
+//     const deleteUrl = window.location.pathname;
+// console.log('clicked');
+//     alert('삭제시 복구할 수 없습니다. 정말 삭제하시겠습니까?');
+//
+//     // DELETE 요청 전송
+//     fetch(deleteUrl, {
+//         method: 'DELETE',
+//     })
+//         .then((response) => {
+//             if (response.ok) {
+//                 location.href = '/board/list';
+//             } else {
+//                 console.log(response.status);
+//                 alert('삭제 실패: ' + response.statusText);
+//             }
+//         })
+//         .catch((error) => {
+//             console.error(error);
+//             alert('오류 발생. 다시 시도해주세요.');
+//         });
+// });
 
-editBtn.addEventListener('click', () => {
+
+function editBoard() {
     const editUrl = location.href + '/update';
 
     // GET 요청 전송
@@ -19,9 +63,9 @@ editBtn.addEventListener('click', () => {
             console.error(error);
             alert('오류 발생. 다시 시도해주세요.');
         });
-});
+}
 
-deleteBtn.addEventListener('click', () => {
+function deleteBoard() {
     const deleteUrl = window.location.pathname;
 
     alert('삭제시 복구할 수 없습니다. 정말 삭제하시겠습니까?');
@@ -42,4 +86,4 @@ deleteBtn.addEventListener('click', () => {
             console.error(error);
             alert('오류 발생. 다시 시도해주세요.');
         });
-});
+}
